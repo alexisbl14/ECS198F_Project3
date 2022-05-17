1d9a6c49f3817949b7e189e37fe0e9de77f96590
Arnav Rastogi <arnavrastogi2002@gmail.com>
2022-05-16 21:42:56 -0700

    attempt 3.2

--- app/src/main/java/com/ecs198f/foodtrucks/FoodTruckReviewsFragment.kt
+++ app/src/main/java/com/ecs198f/foodtrucks/FoodTruckReviewsFragment.kt
@@ -6,14 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ecs198f.foodtrucks.databinding.FragmentFoodTruckReviewsBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class FoodTruckReviewsFragment(private var foodTruck: FoodTruck) : Fragment() {
 
+    private val RC_SIGN_IN = 9001
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,6 +29,25 @@ class FoodTruckReviewsFragment(private var foodTruck: FoodTruck) : Fragment() {
         binding.reviewRecyclerView.apply {
             adapter = recyclerViewAdapter
             layoutManager = LinearLayoutManager(context)
+        }
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestIdToken("699423245763-insnbbp034ep600msiqfan5g0b2pau67.apps.googleusercontent.com")
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        val mGoogleSignInClient = GoogleSignIn.getClient(this.requireContext(), gso)
+
+        fun signIn() {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(
+                signInIntent, RC_SIGN_IN
+            )
+        }
+
+        view?.findViewById<Button>(R.id.signinButton)?.setOnClickListener{
+            signIn()
         }
 
         (requireActivity() as MainActivity).apply {
--- app/src/main/java/com/ecs198f/foodtrucks/FoodTruckService.kt
+++ app/src/main/java/com/ecs198f/foodtrucks/FoodTruckService.kt
@@ -1,7 +1,9 @@
 package com.ecs198f.foodtrucks
 
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface FoodTruckService {
@@ -13,4 +15,7 @@ interface FoodTruckService {
 
     @GET("food-trucks/{id}/reviews")
     fun listFoodReviews(@Path("id") truckId: String): Call<List<Review>>
+
+    @POST("/food-trucks/{id}/reviews")
+    fun postFoodReviews(@Path("id") truckId: String, @Body review: Review): Call<Unit>
 }
